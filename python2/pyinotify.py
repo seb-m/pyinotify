seb-m/pyinotify

commit d334049eed6b1136a9d224a3ca17aa66fea5215a

Refinement of previous commit implemented with help of patch of Guillermo Gonzalez <guillermo.gonzalez@canonical.com>.

python2/pyinotify.py
@@ -730,13 +730,6 @@ class _SysProcessEvent(_ProcessEvent):
         IN_OPEN, IN_DELETE, IN_DELETE_SELF, IN_UNMOUNT.
         """
         watch_ = self._watch_manager.get_watch(raw_event.wd)
-        if watch_ is None:
-            # Not really sure how we ended up here, nor how we should
-            # handle these types of events and if it is appropriate to
-            # completly skip them or not (like we are doing here).
-            log.debug("Unable to retrieve Watch object associated to event %s",
-                      repr(raw_event))
-            return
         if raw_event.mask & (IN_DELETE_SELF | IN_MOVE_SELF):
             # Unfornulately this information is not provided by the kernel
             dir_ = watch_.dir
@@ -1128,9 +1121,14 @@ class Notifier:
         while self._eventq:
             raw_event = self._eventq.popleft()  # pop next event
             watch_ = self._watch_manager.get_watch(raw_event.wd)
-            revent = self._sys_proc_fun(raw_event)  # system processings
-            if revent is None:
+            if watch_ is None:
+                # Not really sure how we ended up here, nor how we should
+                # handle these types of events and if it is appropriate to
+                # completly skip them (like we are doing here).
+                log.warning("Unable to retrieve Watch object associated to %s",
+                            repr(raw_event))
                 continue
+            revent = self._sys_proc_fun(raw_event)  # system processings
             if watch_ and watch_.proc_fun:
                 watch_.proc_fun(revent)  # user processings
             else:

python3/pyinotify.py
@@ -694,13 +694,6 @@ class _SysProcessEvent(_ProcessEvent):
         IN_OPEN, IN_DELETE, IN_DELETE_SELF, IN_UNMOUNT.
         """
         watch_ = self._watch_manager.get_watch(raw_event.wd)
-        if watch_ is None:
-            # Not really sure how we ended up here, nor how we should
-            # handle these types of events and if it is appropriate to
-            # completly skip them or not (like we are doing here).
-            log.debug("Unable to retrieve Watch object associated to event %s",
-                      repr(raw_event))
-            return
         if raw_event.mask & (IN_DELETE_SELF | IN_MOVE_SELF):
             # Unfornulately this information is not provided by the kernel
             dir_ = watch_.dir
@@ -1091,9 +1084,14 @@ class Notifier:
         while self._eventq:
             raw_event = self._eventq.popleft()  # pop next event
             watch_ = self._watch_manager.get_watch(raw_event.wd)
-            revent = self._sys_proc_fun(raw_event)  # system processings
-            if revent is None:
+            if watch_ is None:
+                # Not really sure how we ended up here, nor how we should
+                # handle these types of events and if it is appropriate to
+                # completly skip them (like we are doing here).
+                log.warning("Unable to retrieve Watch object associated to %s",
+                            repr(raw_event))
                 continue
+            revent = self._sys_proc_fun(raw_event)  # system processings
             if watch_ and watch_.proc_fun:
                 watch_.proc_fun(revent)  # user processings
             else:

commit 43f47fed3a28b7cd62cfcf1bcfa948d7499a0407

Correctly handle IN_Q_OVERFLOW events.
This commit should fix a regression introduced with commit
d334049eed6b1136a9d224a3ca17aa66fea5215a.

Issue reported by Tomi Belan @TomiBelan.

python2/pyinotify.py
@@ -1281,10 +1281,10 @@ class Notifier:
         while self._eventq:
             raw_event = self._eventq.popleft()  # pop next event
             watch_ = self._watch_manager.get_watch(raw_event.wd)
-            if watch_ is None:
-                # Not really sure how we ended up here, nor how we should
-                # handle these types of events and if it is appropriate to
-                # completly skip them (like we are doing here).
+            # Not really sure how we ended up here, nor how we should
+            # handle these types of events and if it is appropriate to
+            # completly skip them (like we are doing here).
+            if (watch_ is None) and not (raw_event.mask & IN_Q_OVERFLOW):
                 if not (raw_event.mask & IN_IGNORED):
                     log.warning("Unable to retrieve Watch object associated to %s",
                                 repr(raw_event))

python3/pyinotify.py
@@ -1263,10 +1263,10 @@ class Notifier:
         while self._eventq:
             raw_event = self._eventq.popleft()  # pop next event
             watch_ = self._watch_manager.get_watch(raw_event.wd)
-            if watch_ is None:
-                # Not really sure how we ended up here, nor how we should
-                # handle these types of events and if it is appropriate to
-                # completly skip them (like we are doing here).
+            # Not really sure how we ended up here, nor how we should
+            # handle these types of events and if it is appropriate to
+            # completly skip them (like we are doing here).
+            if (watch_ is None) and not (raw_event.mask & IN_Q_OVERFLOW):
                 if not (raw_event.mask & IN_IGNORED):
                     log.warning("Unable to retrieve Watch object associated to %s",
                                 repr(raw_event))
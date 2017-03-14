3622b0511037b88be038e34b52b72fbb01e7fa03
Dave Johansen <davejohansen@gmail.com>
2017-03-14 11:27:55 -0600

    Allow building on Mac OS X

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ if sys.version_info < (2, 4):
     sys.exit(1)
 
 # check linux platform
-if not platform.startswith('linux') and not platform.startswith('freebsd'):
+if not platform.startswith('linux') and not platform.startswith('freebsd') and not platform.startswith('macosx'):
     sys.stderr.write("inotify is not available on %s\n" % platform)
     sys.exit(1)
 
